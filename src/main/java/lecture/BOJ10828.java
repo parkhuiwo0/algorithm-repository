da3764f224c0642c8d571d5e8d3b5a075dfd27b9
HuiwooPark <huiwo0@naver.com>
2020-07-03 10:57:54 +0900

    백준 알고리즘 10828번 - 스택 자료구조 구현

---
+++ src/main/java/lecture/BOJ10828.java
@@ -0,0 +1,51 @@
+package lecture;
+
+import java.util.Scanner;
+
+/**
+ * 스택 자료구조를 구현
+ * url : https://www.acmicpc.net/problem/10828
+ */
+public class BOJ10828 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] stack = new int[n];
+
+        int size = 0;
+
+        while (n-- > 0) {
+            String cmd = sc.next();
+            if (cmd.equals("push")) {
+                int num = sc.nextInt();
+                stack[size++] = num;
+            } else if (cmd.equals("top")) {
+                if (size == 0) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(stack[size-1]);
+                }
+            } else if (cmd.equals("size")) {
+                System.out.println(size);
+            } else if (cmd.equals("empty")) {
+                if (size == 0) {
+                    System.out.println("1");
+                } else {
+                    System.out.println("0");
+                }
+            } else if (cmd.equals("pop")) {
+                if (size == 0) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(stack[size-1]);
+                    size -= 1;
+                }
+            }
+        }
+
+    }
+}
