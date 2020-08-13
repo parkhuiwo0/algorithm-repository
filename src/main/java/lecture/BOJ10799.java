2eaf867138d05081e886a647168d3ef8cab27123
HuiwooPark <parkhuiwo0@gmail.com>
2020-08-13 11:01:41 +0900

    BOJ10799 : 쇠막대기

---
+++ src/main/java/lecture/BOJ10799.java
@@ -0,0 +1,31 @@
+package lecture;
+
+import java.io.*;
+import java.util.Stack;
+
+public class BOJ10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        String s = br.readLine();
+        int index = 0;
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            index++;
+            if (c == ')') {
+                if (stack.pop()+1 == index) {
+                    count+=stack.size();
+                }
+                else {
+                    count++;
+                }
+            } else {
+                stack.push(index);
+            }
+        }
+        System.out.println(count);
+
+    }
+}
