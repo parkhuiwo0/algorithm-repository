e35be8d183208e0f5578c0fa1a82fcdce1fd9b54
HuiwooPark <parkhuiwo0@gmail.com>
2020-08-14 14:50:07 +0900

    11726번 : 2xN타일링

---
+++ src/main/java/lecture/dynamicProgramming/BOJ11726.java
@@ -0,0 +1,33 @@
+package lecture.dynamicProgramming;
+
+import java.io.*;
+
+/**
+ * acmicpc.net/problem/11726 [2xN 타일링]
+ * 2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+ * D[N] = 2xn 크기의 직사각형을 채우는 방법의 수
+ * 문제에서 제한을 준 케이스.
+ * 가장 오른쪽에 타일을 채우는 방법이 2가지인데 각각 크기가 2xD[N-1]과 2xD[N-2]
+ * D[N] = D[N-1] + D[N-2]
+ */
+public class BOJ11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n;
+        n = Integer.parseInt(br.readLine());
+
+        int[] memorization = new int[n+1];
+        memorization[0] = 1;
+        memorization[1] = 1;
+
+        for (int i =2; i<=n; i++) {
+            memorization[i] = memorization[i-1] + memorization[i-2];
+            memorization[i] %= 10007;
+        }
+
+        System.out.println(memorization[n]);
+
+    }
+
+}
